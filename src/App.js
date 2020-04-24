a62a062f6947e28d82575f939aeb8db99813cfd3
neelesh7singh <neeleshsingh767@gmail.com>
2020-04-24 14:25:05 +0530

    Built basic structure

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import { BrowserRouter } from 'react-router-dom';
+
+
+function App() {
+  return (
+    <BrowserRouter>
+      <div className="App">
+        <h1>Hello</h1>
+      </div>
+    </BrowserRouter>
+  );
+}
+
+export default App;
